18dc0892a7947e64ce731456196249b6b8afff2c
FrostyPlugDj <frostyplugdj@hotmail.com>
2014-03-07 19:03:53 +0000

    Update mubBotPT.js

--- mubBotPT.js
+++ mubBotPT.js
@@ -146,10 +146,10 @@ botMethods.djAdvanceEvent = function(data){
 };
 
     API.on(API.CHAT, function(data){
-        if(data.message.indexOf('!') === 0){
+        if(data.message.indexOf('.') === 0){
             var msg = data.message, from = data.from, fromID = data.fromID;
-            var command = msg.substring(1).split(' ');
-            if(typeof command[2] != "undefined"){
+            var command = msg.substring(1).split('.');
+            if(typeof command[2] .= "undefined"){
                 for(var i = 2; i<command.length; i++){
                     command[1] = command[1] + ' ' + command[i];
                 }
